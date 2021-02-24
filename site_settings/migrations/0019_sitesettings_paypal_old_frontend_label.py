3293dab0c15b245f488fad545dad28e0e54bc50e
Franco de Vita <frankyhung93@gmail.com>
2021-02-25 01:19:04 +0800

    fixed previous commit's migration dependency ordering

--- site_settings/migrations/0019_sitesettings_paypal_old_frontend_label.py
+++ site_settings/migrations/0019_sitesettings_paypal_old_frontend_label.py
@@ -6,7 +6,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('site_settings', '0020_sitesettings_offline_thankyou_text'),
+        ('site_settings', '0021_auto_20210224_1709'),
     ]
 
     operations = [

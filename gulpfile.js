f2e5b64f7d1db71c159e9856f2be6be87c0f799f
Franco de Vita <frankyhung93@gmail.com>
2021-03-19 17:44:03 +0800

    more fixes

--- gulpfile.js
+++ gulpfile.js
@@ -22,7 +22,7 @@ function css() {
       file.stat.mtime = date;
       cb(null, file);
     }))
-    .pipe(rename('main_1_5.css'))
+    .pipe(rename('main_1_6.css'))
     .pipe(gulp.dest(destpath))
 }
 

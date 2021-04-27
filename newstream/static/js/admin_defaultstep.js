286282c99283fff0239b6543b24e9124e7acbde4
Franco de Vita <frankyhung93@gmail.com>
2021-04-27 23:10:23 +0800

    fix admin_defaultstep.js jquery on change bug

--- newstream/static/js/admin_defaultstep.js
+++ newstream/static/js/admin_defaultstep.js
@@ -1,6 +1,6 @@
 window.onload = () => {
     (($) => {
-        $(".is-default-amount-step input").change(function() {
+        $(document).on('change', '.is-default-amount-step input', function() {
             $(".is-default-amount-step input").not(this).prop('checked', false);
         });
     })(window.jQuery)

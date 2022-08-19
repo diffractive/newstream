0d155c127014c137b32f7177c9ba332acade888a
Guil <guilherme.quadros.souza@gmail.com>
2022-08-19 17:51:39 +0800

    Add a sample selenium test

---
+++ notebooks/components.py
@@ -0,0 +1,76 @@
+# +
+from selenium.webdriver.support.ui import Select
+from selenium.common.exceptions import NoSuchElementException
+
+from utils import get_element_by_identifier, get_children_elements
+
+
+# -
+
+class Button:
+    xpath = 'button'
+    def __init__(self, driver, identifier):
+        self.element = get_element_by_identifier(driver, self.xpath, identifier)
+    
+    def click(self):
+        self.element.click()    
+
+
+class Link:
+    xpath = 'a'
+    def __init__(self, driver, identifier):
+        self.element = get_element_by_identifier(driver, self.xpath, identifier)
+    
+    def click(self):
+        self.element.click()
+
+
+class Dropdown:
+    xpath = 'select'
+    def __init__(self, driver, identifier):
+        self.element = Select(get_element_by_identifier(driver, self.xpath, identifier))
+    
+    def select(self, index=0, value=None):
+        if value:
+            try:
+                self.element.select_by_value(value)
+            except NoSuchElementException:
+                pass
+            self.element.select_by_visible_text(value)
+        else:
+            self.element.select_by_index(index)
+
+
+class Input:
+    xpath = 'input'
+    def __init__(self, driver, identifier):
+        self.element = get_element_by_identifier(driver, self.xpath, identifier)
+    
+    def fill(self, value):
+        self.element.send_keys(value)
+
+
+class Application:
+    def __init__(self, driver):
+        self.driver = driver
+        
+    #### Components ####
+    
+    def button(self, identifier):
+        return Button(self.driver, identifier)
+    
+    def link(self, identifier):
+        return Link(self.driver, identifier)
+
+    def dropdown(self, identifier):
+        return Dropdown(self.driver, identifier)
+    
+    def input(self, identifier):
+        return Input(self.driver, identifier)
+    
+    #### Methods ####
+    
+    def go(self, url=''):
+        self.driver.get(f'http://app.newstream.local:8000/{url}')
+
+
--- notebooks/selenium/User Sign Up.py
+++ notebooks/selenium/User Sign Up.py
@@ -18,17 +18,50 @@
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait, Select
 from selenium.webdriver.support import expected_conditions as EC
+from selenium.common.exceptions import NoSuchElementException
 
 from diffractive.selenium import wait_element, ScreenGrabber, get_webdriver, notebook_root
 from diffractive.selenium.visualisation import gallery
 
-import secrets
-# -
+from components import Application
 
+import secrets
+
+# +
 randstr = secrets.token_hex(6).upper()
+
+email = f'test_user{randstr}@newstream.com'
+first_name = 'Test'
+last_name = 'User'
+password = 'strongpwd'
+# -
 
 driver = get_webdriver('portal')
 grabber = ScreenGrabber(driver)
-driver.get('http://app.newstream.local:8000/')
+app = Application(driver)
 
-driver.get('http://app.newstream.local:8000/')
+app.go()
+grabber.capture_screen('home_page', 'Home')
+
+app.link('Donation Form').click()
+grabber.capture_screen('donation_form', 'Donation Form')
+
+# Cannot go to the signup page without setting a value into the custom amount or choosing a default from selector
+app.dropdown('id_donation_amount').select(1)
+app.button('Register or Login').click()
+grabber.capture_screen('register_login', 'Register or login page')
+
+app.link('Continue with Email Sign up').click()
+grabber.capture_screen('sign_up', 'Sign up form')
+
+app.input('id_email').fill(email)
+app.input('id_first_name').fill(first_name)
+app.input('id_last_name').fill(last_name)
+app.input('id_password1').fill(password)
+app.input('id_password2').fill(password)
+grabber.capture_screen('filled_form', 'Filled signup form')
+
+app.button('Continue').click()
+grabber.capture_screen('signed_up', 'Successfully signed up')
+
+gallery(zip(grabber.screens.values(), grabber.captions.values()), row_height="300px")

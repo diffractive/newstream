ed151a94d2cc0c5fd1106b0427eaebfbd6a1c685
guilhbr <guilherme.quadros.souza@gmail.com>
2023-01-11 17:38:09 +0800

    NWST-286 - Create new views for exporting data

--- newstream/views.py
+++ newstream/views.py
@@ -1,12 +1,17 @@
+import json
+import csv
+from datetime import datetime, timezone
 from django.shortcuts import render, redirect
 from django.contrib import messages
 from django.contrib.auth import logout, get_user_model
+from django.http import HttpResponse
 from django.contrib.auth.decorators import login_required
 from django.utils.translation import gettext_lazy as _
+from django.core.exceptions import PermissionDenied
 from django.conf import settings
 from django.utils import translation
 
-from donations.models import Subscription, STATUS_ACTIVE, STATUS_PROCESSING, STATUS_PAUSED
+from donations.models import Subscription, Donation, STATUS_ACTIVE, STATUS_PROCESSING, STATUS_PAUSED
 from donations.email_functions import sendAccountDeletedNotifToAdmins, sendAccountDeletedNotifToDonor
 from newstream.functions import generateIDSecretHash, process_user_meta
 from newstream.forms import PersonalInfoForm, DeleteAccountForm
@@ -101,3 +106,108 @@ def delete_account(request):
     else:
         form = DeleteAccountForm()
     return render(request, 'profile_settings/delete_account.html', {'form': form, 'confirm_text': DeleteAccountForm.CONFIRM_TEXT})
+
+@login_required
+def download_export_data(request):
+    if not request.user.is_staff:
+        raise PermissionDenied
+    return render(request, 'export_data.html')
+
+@login_required
+def export_donation_data(request):
+    """
+    This function is the same as the other export function on the admin site, the main difference is this
+    one does not have any PII on the data
+    """
+    if not request.user.is_staff:
+        raise PermissionDenied
+    filename = datetime.now(timezone.utc).strftime("%Y%m%d-%H%M%S") + 'donation_data.csv'
+    response = HttpResponse(content_type='text/csv')
+    response['Content-Disposition'] = 'attachment; filename=' + filename
+
+    # Include only fields that do not have a PII
+    headings1 = ['id', 'transaction_id','donation_amount', 'is_recurring', 'currency', 'payment_status']
+    headings2 = ['linked_user_deleted', 'gateway', 'subscription_id']
+    headings3 = ['created_at', 'updated_at', 'donation_date',]
+    donations = Donation.objects.all().select_related('user')
+    headings =  headings1 + headings2 + headings3
+
+    writer = csv.writer(response)
+    writer.writerow(headings)
+
+    for donation in donations:
+        data_row = [getattr(donation, field) for field in headings1]
+        data_row = [getattr(donation, field) for field in headings2]
+
+        created_at = getattr(donation, 'created_at').strftime("%Y-%m-%d %H:%M:%S")
+        updated_at = getattr(donation, 'updated_at').strftime("%Y-%m-%d %H:%M:%S")
+        donation_date = getattr(donation, 'donation_date').strftime("%Y-%m-%d")
+        data_row += [created_at, updated_at, donation_date]
+
+        writer.writerow(data_row)
+
+    return response
+
+@login_required
+def export_subscription_data(request):
+    """
+    This function is the same as the other export function on the admin site, the main difference is this
+    one does not have any PII on the data
+    """
+    if not request.user.is_staff:
+        raise PermissionDenied
+    filename = datetime.now(timezone.utc).strftime("%Y%m%d-%H%M%S") + 'subscriptions.csv'
+    response = HttpResponse(content_type='text/csv')
+    response['Content-Disposition'] = 'attachment; filename=' + filename
+
+    headings1 = ['id', 'profile_id','recurring_amount', 'currency', 'recurring_status']
+    headings2 = ['linked_donor_deleted', 'gateway']
+    headings3 = ['created_at', 'updated_at', 'subscribe_date']
+    subscriptions = Subscription.objects.all().select_related('user')
+    headings =  headings1 + headings2 + headings3
+
+    writer = csv.writer(response)
+    writer.writerow(headings)
+    for subscription in subscriptions:
+        data_row = [getattr(subscription, field) for field in headings1]
+        data_row = [getattr(subscription, field) for field in headings2]
+
+        created_at = getattr(subscription, 'created_at').strftime("%Y-%m-%d %H:%M:%S")
+        updated_at = getattr(subscription, 'updated_at').strftime("%Y-%m-%d %H:%M:%S")
+        subscribe_date = getattr(subscription, 'subscribe_date').strftime("%Y-%m-%d")
+
+        data_row += [created_at, updated_at, subscribe_date]
+
+        writer.writerow(data_row)
+
+    return response
+
+@login_required
+def export_donor_data(request):
+    """
+    This function is the same as the other export function on the admin site, the main difference is this
+    one does not have any PII on the data
+    """
+    if not request.user.is_staff:
+        raise PermissionDenied
+    filename = datetime.now(timezone.utc).strftime("%Y%m%d-%H%M%S") + 'donors.csv'
+    response = HttpResponse(content_type='text/csv')
+    response['Content-Disposition'] = 'attachment; filename=' + filename
+
+    donors = User.objects.all()
+    headings1 = ['id', 'is_active']
+    headings2 = ['date_joined']
+    headings = headings1 + headings2
+
+    writer = csv.writer(response)
+    writer.writerow(headings)
+    for donor in donors:
+        data_row = ([getattr(donor, field) for field in headings1])
+        date_joined = getattr(donor, 'date_joined').strftime("%Y-%m-%d %H:%M:%S")
+        data_row += [date_joined]
+        writer.writerow(data_row)
+    guests = Donation.objects.order_by().values_list('guest_email', flat=True).distinct()
+    for guest in guests:
+        if guest:
+            writer.writerow(['GUEST','',''])
+    return response

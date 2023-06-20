302f4b9dcb1afd20c3ad922ee715afa2845a1905
Franky Hung <franky@diffractive.io>
2023-06-20 16:27:10 +0900

    updated test_connection with screencap, fixed missing initdb and schema scripts for gcp compose

--- notebooks/selenium/Donations/PayPal/test_connection.py
+++ notebooks/selenium/Donations/PayPal/test_connection.py
@@ -14,12 +14,21 @@
 #     name: python3
 # ---
 
-# +
 import os
 import requests
+from diffractive.selenium import ScreenGrabber, get_webdriver, notebook_root
+from diffractive.selenium.visualisation import gallery
+from components import Application
 
-# -
+driver = get_webdriver('portal')
+grabber = ScreenGrabber(driver)
+app = Application(driver)
+
+driver.get(os.getenv('PAYPAL_API_BASE'))
+grabber.capture_screen('paypal_sample_api', 'Sample API response')
+
 # should just return a list of users saved on the localpaypal database
 req = requests.get(os.getenv('PAYPAL_API_BASE'))
 assert req.status_code == 200
 
+gallery(zip(grabber.screens.values(), grabber.captions.values()), row_height="300px")

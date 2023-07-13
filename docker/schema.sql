ca9fcd19fecc55c6973a3a9b6beb3e02a8871517
Franky Hung <franky@diffractive.io>
2023-07-13 16:48:33 +0900

    NWST-371: updated schema, added PAYPAL_API_BASE for pointing to localpaypal and added ui test for subscription flow with localpaypal

--- docker/schema.sql
+++ docker/schema.sql
@@ -1,8 +1,95 @@
-CREATE TABLE accounts (
-    user_id serial PRIMARY KEY,
-    username VARCHAR(50) UNIQUE NOT NULL,
-    password VARCHAR(50) NOT NULL,
-    email VARCHAR(255) UNIQUE NOT NULL
+CREATE TABLE "user" (
+    id VARCHAR(50) PRIMARY KEY,
+    email VARCHAR(255) UNIQUE NOT NULL,
+    password VARCHAR(255) NOT NULL,
+    given_name VARCHAR(255) NOT NULL,
+    surname VARCHAR(255) NOT NULL,
+    card_number CHAR(16),
+    card_expiry DATE,
+    card_cvv CHAR(3),
+    create_time TIMESTAMP NOT NULL default current_timestamp,
+    update_time TIMESTAMP NOT NULL
 );
 
-INSERT INTO accounts (username, password, email) VALUES ('franky', 'franky', 'franky@diffractive.io');
+CREATE TABLE "payment" (
+    id VARCHAR(50) PRIMARY KEY,
+    amount VARCHAR(50) NOT NULL,
+    currency_code VARCHAR(3) NOT NULL,
+    status VARCHAR(255) NOT NULL default 'PENDING',
+    create_time TIMESTAMP NOT NULL default current_timestamp,
+    update_time TIMESTAMP NOT NULL
+);
+
+CREATE TABLE "order" (
+    id VARCHAR(50) PRIMARY KEY,
+    processing_instruction VARCHAR(36) default 'NO_INSTRUCTION',
+    purchase_units JSON NOT NULL,
+    payment_source JSON NOT NULL,
+    intent VARCHAR(50) NOT NULL,
+    payer JSON,
+    status VARCHAR(255) NOT NULL default 'CREATED',
+    create_time TIMESTAMP NOT NULL default current_timestamp,
+    update_time TIMESTAMP NOT NULL
+);
+
+CREATE TABLE "product" (
+    id VARCHAR(50) PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
+    description VARCHAR(255),
+    type VARCHAR(50) default 'PHYSICAL',
+    category VARCHAR(255),
+    create_time TIMESTAMP NOT NULL default current_timestamp,
+    update_time TIMESTAMP NOT NULL
+);
+
+CREATE TABLE "plan" (
+    id VARCHAR(50) PRIMARY KEY,
+    product_id VARCHAR(50) NOT NULL references product(id),
+    name VARCHAR(255) NOT NULL,
+    billing_cycles JSON NOT NULL,
+    payment_preferences JSON NOT NULL,
+    status VARCHAR(50) NOT NULL default 'CREATED',
+    create_time TIMESTAMP NOT NULL default current_timestamp,
+    update_time TIMESTAMP NOT NULL
+);
+
+CREATE TABLE "subscription" (
+    id VARCHAR(50) PRIMARY KEY,
+    plan_id VARCHAR(50) NOT NULL references plan(id),
+    quantity VARCHAR(50) default '1',
+    custom_id VARCHAR(255),
+    start_time TIMESTAMP NOT NULL default current_timestamp,
+    subscriber JSON,
+    billing_info JSON,
+    status VARCHAR(255) NOT NULL default 'APPROVAL_PENDING',
+    create_time TIMESTAMP NOT NULL default current_timestamp,
+    update_time TIMESTAMP NOT NULL
+);
+
+CREATE TABLE "checkout_session" (
+    id VARCHAR(50) PRIMARY KEY,
+    subscription_id VARCHAR(50) references subscription(id),
+    order_id VARCHAR(50) references "order" (id),
+    payment_id VARCHAR(50) references payment(id),
+    return_url TEXT NOT NULL,
+    cancel_url TEXT NOT NULL,
+    status VARCHAR(255) NOT NULL default 'PENDING',
+    create_time TIMESTAMP NOT NULL default current_timestamp,
+    update_time TIMESTAMP NOT NULL
+);
+
+CREATE TABLE "webhook" (
+    id VARCHAR(50) PRIMARY KEY,
+    url TEXT NOT NULL,
+    event_types JSON NOT NULL,
+    create_time TIMESTAMP NOT NULL default current_timestamp,
+    update_time TIMESTAMP NOT NULL
+);
+
+CREATE TABLE "oauth_token" (
+    id VARCHAR(50) PRIMARY KEY,
+    access_token VARCHAR(255) UNIQUE NOT NULL,
+    expires_in INTEGER NOT NULL,
+    token_type VARCHAR(50) NOT NULL,
+    create_time TIMESTAMP NOT NULL default current_timestamp
+);
---
+++ notebooks/selenium/Donations/PayPal/User Subscription.py
@@ -0,0 +1,133 @@
+# -*- coding: utf-8 -*-
+# ---
+# jupyter:
+#   jupytext:
+#     formats: ipynb,py:light
+#     text_representation:
+#       extension: .py
+#       format_name: light
+#       format_version: '1.5'
+#       jupytext_version: 1.11.4
+#   kernelspec:
+#     display_name: Python 3 (ipykernel)
+#     language: python
+#     name: python3
+# ---
+
+# +
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait, Select
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.common.exceptions import NoSuchElementException
+
+from diffractive.selenium import wait_element, ScreenGrabber, get_webdriver, notebook_root
+from diffractive.selenium.visualisation import gallery
+
+from components import Application
+from utils import get_email_count, wait_for_email, get_emails, clear_all_emails
+
+import secrets
+
+# +
+clear_all_emails()
+randstr = secrets.token_hex(6).upper()
+
+email = f'test_user{randstr}@newstream.com'
+first_name = 'Test'
+last_name = 'User'
+password = 'strongpwd'
+name = 'Test User'
+card_number = '4242424242424242'
+card_expiry = '1133'
+cvc = '123'
+email_count = get_email_count()
+# -
+
+driver = get_webdriver('portal')
+grabber = ScreenGrabber(driver)
+app = Application(driver)
+
+app.go()
+grabber.capture_screen('home_page', 'Home')
+
+app.link('Donation Form').click()
+grabber.capture_screen('donation_form', 'Donation Form')
+
+# +
+# Cannot go to the signup page without setting a value into the custom amount or choosing a default from selector
+app.dropdown('id_donation_amount').select('HKD $100')
+app.dropdown('id_donation_frequency').select('Monthly')
+app.dropdown('id_payment_gateway').select('PayPal')
+
+# We need to first sign up with an account in order to use a user payment
+app.button('Register or Login').click()
+grabber.capture_screen('sign_in_sign_up', 'Sign in or Sign up page')
+# -
+
+app.link('Continue with Email Sign up').click()
+grabber.capture_screen('sign_up', 'Sign up form')
+
+app.input('id_email').fill(email)
+app.input('id_first_name').fill(first_name)
+app.input('id_last_name').fill(last_name)
+app.input('id_password1').fill(password)
+app.input('id_password2').fill(password)
+grabber.capture_screen('filled_form', 'Filled signup form')
+
+app.button('Continue').click()
+grabber.capture_screen('signed_up', 'Successfully signed up')
+
+# +
+# There should be two emails sent, one for admins one for the user
+wait_for_email(email_count+1)
+emails = get_emails(0, 2)
+user_email = 'Please Confirm Your Email Address'
+admin_email = 'A Donor Account is created'
+
+# Email order is not guaranteed
+email_titles = [admin_email, user_email]
+for email_content in emails:
+    email_title = email_content['Content']['Headers']['Subject'][0]
+    email_recipient = email_content['Content']['Headers']['To'][0]
+
+    assert email_title in email_titles, f'Unexpected e-mail found: {email_title}'
+    if email_title == user_email:
+        assert email_recipient == email, \
+            f"Unexpected e-mail recipient {email_recipient}, expected: {email}"
+    email_titles.remove(email_title)
+
+email_count += 2
+# -
+
+app.button('Confirm Donation').click()
+grabber.capture_screen('processing_payment', 'Processing Payment')
+
+# Wait until redirecting finishes
+wait_element(driver, '//input[@id="username"]')
+grabber.capture_screen('paypal_checkout_login', 'PayPal checkout login')
+
+app.link('secondary-btn').click()
+
+wait_element(driver, '//input[@id="card_number"]')
+grabber.capture_screen('paypal_checkout_card_details', 'PayPal checkout card details')
+
+app.input('card_number').fill(card_number)
+app.input('card_expiry').fill(card_expiry)
+app.input('cvc').fill(cvc)
+app.input('primary-btn').click()
+
+wait_element(driver, '//input[@value="agree & subscribe"]')
+grabber.capture_screen('paypal_checkout_subscribe', 'PayPal checkout subscribe')
+
+app.input('primary-btn').click()
+
+wait_element(driver, '//h1[text()="Thank you!"]')
+grabber.capture_screen('thank_you', 'Thank you screen')
+
+# +
+# TODO: implement email checking after webhooks at localpaypal are implemented
+# There should be two emails sent, one for admins one for the user
+
+gallery(zip(grabber.screens.values(), grabber.captions.values()), row_height="300px")
+
+

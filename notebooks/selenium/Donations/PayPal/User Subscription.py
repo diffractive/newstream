69caee22cf5c523fd5083de0a1bbe9945706e2d0
Franky Hung <franky@diffractive.io>
2023-07-17 14:59:22 +0900

    added email checking at last step of User Subscription ui test for PayPal

--- notebooks/selenium/Donations/PayPal/User Subscription.py
+++ notebooks/selenium/Donations/PayPal/User Subscription.py
@@ -128,9 +128,24 @@ grabber.capture_screen('thank_you', 'Thank you screen')
 assert driver.find_element(By.ID, 'payment_status').text == "Complete"
 assert driver.find_element(By.ID, 'recurring_status').text == "Active"
 
-# +
-# TODO: implement email checking after webhooks at localpaypal are implemented
 # There should be two emails sent, one for admins one for the user
+wait_for_email(email_count+1)
+emails = get_emails(0, 2)
+user_email = 'Thank you for setting up a Recurring Donation'
+admin_email = 'New Recurring Donation'
+
+# Email order is not guaranteed
+email_titles = [admin_email, user_email]
+for email_content in emails:
+    email_title = email_content['Content']['Headers']['Subject'][0]
+    email_recipient = email_content['Content']['Headers']['To'][0]
+
+    assert email_title in email_titles, f'Unexpected e-mail found: {email_title}'
+    if email_title == user_email:
+        assert email_recipient == email, \
+            f"Unexpected e-mail recipient {email_recipient}, expected: {email}"
+    email_titles.remove(email_title)
+# -
 
 gallery(zip(grabber.screens.values(), grabber.captions.values()), row_height="300px")
 # -

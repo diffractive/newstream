3a2dc957f67f77d62850acdaa97a09a51552d06e
Franky Hung <franky@diffractive.io>
2023-07-19 19:11:15 +0900

    fix unclean merge conflicts

--- notebooks/selenium/Donations/PayPal/Failed Subscription Reactivated by itself.py
+++ notebooks/selenium/Donations/PayPal/Failed Subscription Reactivated by itself.py
@@ -89,8 +89,6 @@ paypal.simulate_next_payment_cycle(sub_id, "failure")
 app.go('en/donations/my-recurring-donations/')
 rows = app.table('my-donations-table').row_values()
 assert rows[0][5] == 'Payment failed'
-<<<<<<< HEAD:notebooks/selenium/Donations/PayPal/Test Subscription Renewals.py
-=======
 grabber.capture_screen('payment_failed', 'Failed payment subscriptions')
 
 paypal.simulate_next_payment_cycle(sub_id, "success")
@@ -98,6 +96,5 @@ app.go('en/donations/my-recurring-donations/')
 rows = app.table('my-donations-table').row_values()
 assert rows[0][5] == 'Active'
 grabber.capture_screen('successful_payment', 'Subscription is now successful again')
->>>>>>> main:notebooks/selenium/Donations/PayPal/Failed Subscription Reactivated by itself.py
 
 gallery(zip(grabber.screens.values(), grabber.captions.values()), row_height="300px")

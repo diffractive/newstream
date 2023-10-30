be37125aeccc9569e6f0418966234568f3384e0c
guilhbr <guilherme.quadros.souza@gmail.com>
2023-10-30 13:26:04 +0800

    NWST-402 - serve static files with whitenoise

--- django-app/newstream/settings/base.py
+++ django-app/newstream/settings/base.py
@@ -306,7 +306,7 @@ STATICFILES_DIRS = [
 # ManifestStaticFilesStorage is recommended in production, to prevent outdated
 # Javascript / CSS assets being served from cache (e.g. after a Wagtail upgrade).
 # See https://docs.djangoproject.com/en/3.0/ref/contrib/staticfiles/#manifeststaticfilesstorage
-# STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.ManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'

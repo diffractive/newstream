0c2ee8799a608282bc17006bc2e335ed68dda199
guilhbr <guilherme.quadros.souza@gmail.com>
2023-11-10 14:46:59 +0800

    NWST-417 - Change server error to 404

--- django-app/newstream/gcsmedia.py
+++ django-app/newstream/gcsmedia.py
@@ -1,7 +1,12 @@
+import logging
+logger = logging.getLogger('newstream')
 from django.core.files.storage import default_storage
-from django.http import FileResponse
+from django.http import FileResponse, HttpResponse
 
 
 def gcsmedia(request, path):
-    fileobj = default_storage.open(path)
-    return FileResponse(fileobj)
+    try:
+        fileobj = default_storage.open(path)
+        return FileResponse(fileobj)
+    except FileNotFoundError:
+        return HttpResponse(status=404)

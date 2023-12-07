6f323f4bf6eb6c834d1bdfcbbc6322bbbc7d0cbd
guilhbr <guilherme.quadros.souza@gmail.com>
2023-12-07 15:52:26 +0800

    Fix inspect and settings pages

--- django-app/newstream/static/js/admin_inspect.js
+++ django-app/newstream/static/js/admin_inspect.js
@@ -2,7 +2,7 @@ window.onload = () => {
     (($) => {
         var firstTab = $('.tab-nav li a').first();
         if (firstTab) {
-            setTimeout(() => firstTab.tab('show'), 10);
+            setTimeout(() => $(firstTab).show(), 10);
             window.history.replaceState(null, null, $(firstTab).attr('href'));
         }
     })(window.jQuery)

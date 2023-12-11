22c2436394780498574a5572e7d4f6d5284660f9
guilhbr <guilherme.quadros.souza@gmail.com>
2023-12-11 15:03:20 +0800

    Remove wagtail.contrib.admin

--- django-app/newstream/settings/base.py
+++ django-app/newstream/settings/base.py
@@ -102,7 +102,6 @@ INSTALLED_APPS = [
     'wagtail.contrib.forms',
     'wagtail.contrib.redirects',
     'wagtail.contrib.settings',
-    'wagtail.contrib.modeladmin',
     'wagtail.embeds',
     'wagtail.sites',
     'wagtail.users',
